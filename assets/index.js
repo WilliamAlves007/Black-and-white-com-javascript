d51e1172c4cfd6bc26a6c95c13dcd0348b42af4c
William Alves <109997418+WilliamAlves007@users.noreply.github.com>
2024-05-13 20:00:48 -0300

    Delete assets directory

--- assets/index.js
+++
@@ -1,9 +0,0 @@
-
-let corredor = document.getElementById('corredor')
-let body = document.querySelector('body')
-
-
-corredor.addEventListener('click', () => {
-    corredor.classList.toggle('black')
-    body.classList.toggle('black')
-})
